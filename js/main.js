72a7dad1a757fc1f4638b24414efef71f94b5fb4
yuna-c <yuna744@gmail.com>
2023-11-14 10:59:06 +0900

    1114 부모박스에 on이 붙었을 때 모션처리

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const main = document.querySelector('main');
 const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
-const tits = main.querySelectorAll('h1');
+const tits = main.querySelectorAll('h1'); //dom
 
 splitText(tits[0]); //인수로 전달되는 값은 'h1'문자가 아닌 h1 Dom자체를 전달
 splitText(tits[1]);
@@ -37,7 +37,7 @@ function activation(arr, idx) {
 // 해당함수는 DOM자체를 인수로 전달받음
 function splitText(el) {
 	//const dom = document.querySelector(el); //문자열 들어가야 DOM에서 바로 찾을 수 있게
-	// 전달된 dom 이 innerhtml로 받아지게
+	// 전달된 dom 이 innerHtml로 받아지게
 	let tags = '';
 
 	for (let letter of el.innerText) {
