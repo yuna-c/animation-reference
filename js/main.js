ad5269d4b846d19d0295dc570d5597ea13824b07
yuna-c <yuna744@gmail.com>
2023-11-14 10:48:28 +0900

    1114 splitText함수에 선택자 문자열이 DOM자체가 전달되도록 수정

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
-/*
-for of 반복문으로 문자마다 span으로 분리해주는 기능 구현
-const h1 = document.querySelector('h1');
-const txt = h1.innerText;
-let tags = '';
+//tab 구현
+const main = document.querySelector('main');
+const btns = main.querySelectorAll('li');
+const boxs = main.querySelectorAll('article');
+const tits = main.querySelectorAll('h1');
 
-//반복 : 배열,문자도 가능 ES6이후
-for (let letter of txt) {
-	//console.log(letter);
-	tags += `<span>${letter}</span>`;
+splitText(tits[0]); //인수로 전달되는 값은 'h1'문자가 아닌 h1 Dom자체를 전달
+splitText(tits[1]);
+
+// 변수값 not defind
+// console.log(btns);
+// console.log(boxs);
+
+btns.forEach((btn, idx) => {
+	// console.log(btn);
+	// console.log(idx);
+
+	btn.addEventListener('click', () => {
+		/*
+		btns.forEach((btn) => btn.classList.remove('on'));
+		btns[idx].classList.add('on');
+
+		boxs.forEach((btn) => btn.classList.remove('on'));
+		boxs[idx].classList.add('on');
+    */
+
+		activation(btns, idx);
+		activation(boxs, idx);
+	});
+});
+
+function activation(arr, idx) {
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[idx].classList.add('on');
 }
-console.log(tags);
-h1.innerHTML = tags;
-*/
 
-splitText('h1');
-splitText('h2');
-splitText('h3');
-//미션 위의 내용을 처리해주는 함수 제작 (인수로 선택자 전달)
+// 해당함수는 DOM자체를 인수로 전달받음
 function splitText(el) {
-	const dom = document.querySelector(el); //지역변수
-	//const txt = dom.innerText;
+	//const dom = document.querySelector(el); //문자열 들어가야 DOM에서 바로 찾을 수 있게
+	// 전달된 dom 이 innerhtml로 받아지게
 	let tags = '';
 
-	for (let letter of dom.innerText) {
+	for (let letter of el.innerText) {
 		tags += `<span>${letter}</span>`;
 	}
 	console.log(tags);
-	dom.innerHTML = tags;
-
-	// 기능 실행은 return 필요 없어
+	el.innerHTML = tags;
 }
